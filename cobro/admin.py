fa66ec30ad48697867c9756c3238c5761be2dee7
Christopher <cristpohereduardo2004@gmail.com>
2026-05-07 15:14:47 -0500

    tercero Comit

--- cobro/admin.py
+++ cobro/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# 
 from .models import Cliente
 from .models import Cobro
 
 admin.site.register(Cliente)
-admin.site.register(Cobro)
